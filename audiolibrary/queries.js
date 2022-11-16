9cc526675609dbba939d747d38f4ab7b17119870
Jakub Wojtaś <kubawojtas34@gmail.com>
2022-11-16 22:00:27 +0100

    test push

--- audiolibrary/queries.js
+++ audiolibrary/queries.js
@@ -1,6 +1,6 @@
 const client = require("./dbconnection");
 
-const getAircraft = (req, res)=> {
+const getAircraft = (req, res) => {
     client.query(`Select * from track`, (err, result) => {
         if (!err) {
             res.send(result.rows);
@@ -24,10 +24,23 @@ const getAllArtist = (req, res) => {
 const getArtistById = (req, res) => {
     client.query('SELECT * FROM Artist WHERE id='+ req.params.id)
         .then((result) => {
-            res.send(result.rows);
+            res.sendStatus(200).send(result.rows);
             console.log("Find artist with id " + req.params.id)
         })
         .catch((err) => {
+            res.sendStatus(400).json({'message': "Bad request"});
+            console.error(err.stack);
+        })
+    client.end;
+}
+
+const getAllTracksByArtistId = (req, res) => {
+    client.query('SELECT * FROM Track WHERE artist_id=' + req.params.artistId)
+        .then((result) => {
+            res.sendStatus(200).send(result.rows);
+        })
+        .catch((err) => {
+            res.sendStatus(400).send("Bad request");
             console.error(err.stack);
         })
     client.end;
