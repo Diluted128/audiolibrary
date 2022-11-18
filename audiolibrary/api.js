156146423481e079441eaafc415d76f027f641d9
Jakub Wojtaś <kubawojtas34@gmail.com>
2022-11-18 09:36:23 +0100

    finished endpoints

--- audiolibrary/api.js
+++ audiolibrary/api.js
@@ -1,9 +1,10 @@
 const client = require('./dbconnection.js')
 const express = require('express');
-const queries = require('./queries')
+const queries = require('./queries');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("Sever is now listening at port 3000");
 })
 
@@ -11,30 +12,12 @@ client.connect();
 
 //const bodyParser = require("body-parser");
 // app.use(bodyParser.json());
-app.get('/artists', queries.getAllArtist)
-app.get('/artist/:id', queries.getArtistById)
+app.get('/artists', queries.getAllArtists);
+// app.get('/artist/:id', queries.getArtistInfoById);
+app.get('/artist/:id/albums', queries.getAllAlbumsByArtistId);
+app.get('/tracks', queries.getAllTracks);
+app.get('/track/:id', queries.getTrackById);
+app.get('/album/:id/tracks', queries.getTrackByAlbumId);
+app.get('/artist/:id/tracks', queries.getTrackByArtistId);
 
-app.get('/aiecraft', queries.getAircraft)
-
-app.get('/users', (req, res)=>{
-    client.query(`Select * from users where id=${req.params.id}`, (err, result)=>{
-        if(!err){
-            res.send(result.rows);
-        }
-    });
-    client.end;
-})
-
-// app.post('/users', (req, res)=> {
-//     const user = req.body;
-//     let insertQuery = `insert into users(id, firstname, lastname, location)
-//                        values(${user.id}, '${user.firstname}', '${user.lastname}', '${user.location}')`
-//
-//     client.query(insertQuery, (err, result)=>{
-//         if(!err){
-//             res.send('Insertion was successful')
-//         }
-//         else{ console.log(err.message) }
-//     })
-//     client.end;
-// })
+app.post('/album', bodyParser.json(), queries.postAlbum);
--- audiolibrary/queries.js
+++ audiolibrary/queries.js
@@ -1,55 +1,184 @@
 const client = require("./dbconnection");
 
-const getAircraft = (req, res) => {
-    client.query(`Select * from track`, (err, result) => {
-        if (!err) {
-            res.send(result.rows);
-        }
-    });
-    client.end;
-};
+const getAllArtistsQuery = async () => {
+    var query = await client.query('Select * from Artist');
+    return query;
+}
 
-const getAllArtist = (req, res) => {
-    client.query('Select * from Artist')
-        .then((result) => {
-            res.send(result.rows);
-            console.log('Find all artists')
-        })
+const getAllTracksQuery = async () => {
+    var query = await client.query('Select * from Tracks');
+    return query;
+}
+
+const getAllAlbumsQuery = async () => {
+    var query = await client.query('Select * from Album');
+    return query;
+}
+
+const getArtistByIdQuery = async (id) => {
+    var response = await client.query('SELECT * FROM Artist WHERE id=' + id);
+    return response;
+}
+
+const getTrackByIdQuery = async (id) => {
+    var response = await client.query('SELECT * FROM Track WHERE id=' + id);
+    return response;
+}
+
+const getAlbumsByArtistIdQuery = async (id) => {
+    var response = await client.query('SELECT * FROM Album WHERE artist_id=' + id);
+    return response;
+}
+
+const getTracksByAlbumIdQuery = async (id) => {
+    var response = await client.query('SELECT * FROM Track WHERE album_id=' + id);
+    return response;
+}
+
+const pushToAlbumQuery = async (name, artist_id) => {
+    return await client.query('INSERT INTO Album(name, artist_id) VALUES ("' + name + '",'+artist_id+')')
+        .then((result) => console.log("success push new album to database"))
+        .catch((err) => console.error("Failed push album to database"));
+}
+
+const getAllArtists = (req, res) => {
+    getAllArtistsQuery().then((result) => {
+        res.send(result.rows);
+        console.log('Find all artists');
+    }).catch((err) => {
+        console.error(err.stack);
+    })
+    client.end();
+}
+
+const getAllTracks = (req, res) => {
+    getAllTracksQuery().then((result) => {
+        res.send(result.rows);
+        console.log('Find all tracks');
+    })
         .catch((err) => {
             console.error(err.stack);
         })
-    client.end;
+    client.end();
+}
+
+const getAllAlbums = (req, res) => {
+    getAllAlbumsQuery().then((result) => {
+        res.send(result.rows);
+        console.log('Find all artists');
+    })
+        .catch((err) => {
+            console.error(err.stack);
+        })
+    client.end();
 }
 
 const getArtistById = (req, res) => {
-    client.query('SELECT * FROM Artist WHERE id='+ req.params.id)
+    getArtistByIdQuery(req.params.id)
         .then((result) => {
-            res.sendStatus(200).send(result.rows);
-            console.log("Find artist with id " + req.params.id)
+            res.send(result.rows);
+            console.log("Find artist with id " + req.params.id);
         })
         .catch((err) => {
-            res.sendStatus(400).json({'message': "Bad request"});
-            console.error(err.stack);
+            res.sendStatus(404).json({'message': "Request error"})
         })
-    client.end;
+    client.end();
 }
 
-const getAllTracksByArtistId = (req, res) => {
-    client.query('SELECT * FROM Track WHERE artist_id=' + req.params.artistId)
+const getTrackByAlbumId = (req, res) => {
+    getTracksByAlbumIdQuery(req.params.id)
         .then((result) => {
-            res.sendStatus(200).send(result.rows);
+            res.send(result.rows);
+            console.log("Find track with album_id " + req.params.id);
         })
         .catch((err) => {
-            res.sendStatus(400).send("Bad request");
+            res.sendStatus(404).json({'message': "Request error"})
+        })
+    client.end();
+}
+
+const getTrackByArtistId = (req, res) => {
+    getTrackByArtistId(req.params.id)
+        .then((result) => {
+            res.send(result.rows);
+            console.log("Find track with artist_id " + req.params.id);
+
+        })
+        .catch((err) => {
+            res.sendStatus(404).json({'message': "Request error"})
+        })
+    client.end();
+}
+
+const getTrackById = (req, res) => {
+    getTrackByIdQuery(req.params.id)
+        .then((result) => {
+            res.send(result.rows);
+            console.log("Find track with id " + req.params.id)
+        })
+        .catch((err) => {
+            res.sendStatus(404).json({'message': "Request error"})
+        })
+}
+
+const getAllAlbumsByArtistId = (req, res) => {
+    getAlbumsByArtistIdQuery(req.params.id)
+        .then((result) => {
+            res.send(result.rows);
+            console.log('Find all artists');
+        })
+        .catch((err) => {
             console.error(err.stack);
         })
-    client.end;
+}
+
+const getArtistInfoById = (req, res) => {
+    var response = getArtistByIdQuery(req.params.id)
+        .then((result) => {
+            console.log(result.rows)
+            result.rows.forEach((row) => {
+                getAlbumsByArtistIdQuery(row.id)
+                    .then(result => {
+                        console.log(result.rows)
+                        result.rows.forEach((row) => {
+                            getTracksByAlbumIdQuery(row.id)
+                                .then((result) => {
+                                    console.log(result.rows)
+                                })
+                                .catch((err) => {console.log(err.stack)})
+                        })
+                    })
+                    .catch((err) => {console.log(err.stack)})
+            })
+        })
+        .catch((err) => {
+            console.log("not found" + err.stack)
+        });
+    res.sendStatus(404);
+}
+
+const postAlbum = (req, res) => {
+    pushToAlbumQuery(req.body.name, req.body.artist_id)
+        .then((result) => {
+            console.log(result);
+            res.sendStatus(200);
+        })
+        .catch(r => {
+            console.log(r.stack);
+            res.sendStatus(400);});
 }
 
 const queries = {
-    getAircraft,
-    getAllArtist,
-    getArtistById
+    getAllTracks,
+    getAllArtists,
+    getAllAlbums,
+    getArtistById,
+    getArtistInfoById,
+    getTrackByAlbumId,
+    getTrackByArtistId,
+    getTrackById,
+    getAllAlbumsByArtistId,
+    postAlbum
 }
 
 module.exports = queries
